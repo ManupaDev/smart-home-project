e05db32b7bad49797a955afaec1f4ad413f1cc18
ManupaDev <manupadevsuite@gmail.com>
2023-06-24 16:55:10 +0530

    feat: scaffold

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import "./App.css";
 
 function App() {
-
-  const username = "Manupa"; 
-  const age = 22;
-
   return (
-    <div className="container">
-      <h1 className="greeting">Hello, {username}, You are {age} years old</h1>
-      <h3>Your devices are under your control.</h3>
+    <div className="App">
+      <div className="sidebar"></div>
+      <div className="widgets"></div>
+      <div className="home"></div>
     </div>
   );
 }
