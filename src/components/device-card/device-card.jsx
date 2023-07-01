ada35efa63a780c203ee0f0cdfef4b17eff09699
ManupaDev <manupadevsuite@gmail.com>
2023-07-01 16:51:42 +0530

    feat: added prop-types

--- src/components/device-card/device-card.jsx
+++ src/components/device-card/device-card.jsx
@@ -1,18 +1,20 @@
 import s from "./device-card.module.css";
+import PropTypes from "prop-types";
 
 function DeviceCard(props) {
   return (
     <div className={s.device_card}>
-      <img
-        className={s.device_card_img}
-        src={props.image}
-        alt=""
-      />
+      <img className={s.device_card_img} src={props.image} alt="" />
       <div className={s.device_card_info}>
         <h1 className={s.device_card_text}>{props.name}</h1>
       </div>
     </div>
   );
 }
+
+DeviceCard.propTypes = {
+  image: PropTypes.string,
+  name: PropTypes.string,
+};
 
 export default DeviceCard;
