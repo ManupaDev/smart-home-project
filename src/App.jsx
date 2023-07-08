9045104db1673f4f7483068a24a6080821b8ffe2
ManupaDev <manupadevsuite@gmail.com>
2023-07-08 23:18:51 +0530

    feat: day-4 content complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,47 @@
 import "./App.css";
 import Header from "./components/header/header";
 import DeviceCard from "./components/device-card/device-card";
+import LocationChip from "./components/location-chip/location-chip";
+import { useState } from "react";
 
 function App() {
   const devices = [
-    { image: "https://source.unsplash.com/KP7p0-DRGbg", name: "Main Light" },
-    { image: "https://source.unsplash.com/sO5LtzSHpDQ", name: "Ceiling Fan" },
-    { image: "https://source.unsplash.com/toX2sYnycCw", name: "Night Light" },
-    { image: "https://source.unsplash.com/ujSsIk5iZmA", name: "CCTV" },
+    {
+      image: "https://source.unsplash.com/KP7p0-DRGbg",
+      location: "Living Room",
+      name: "Main Light",
+    },
+    {
+      image: "https://source.unsplash.com/sO5LtzSHpDQ",
+      location: "Living Room",
+      name: "Ceiling Fan",
+    },
+    {
+      image: "https://source.unsplash.com/toX2sYnycCw",
+      location: "Bed Room",
+      name: "Night Light",
+    },
+    {
+      image: "https://source.unsplash.com/ujSsIk5iZmA",
+      location: "Living Room",
+      name: "CCTV",
+    },
   ];
+
+  const locations = ["All", "Living Room", "Bed Room"];
+
+  const [selectedLocation, setSelectedLocation] = useState("All");
+
+  const handleLocationSelect = (location) => {
+    setSelectedLocation(location);
+  };
+
+  const filteredDevices =
+    selectedLocation === "All"
+      ? devices
+      : devices.filter((device) => {
+          return device.location === selectedLocation;
+        });
 
   return (
     <div className="App">
@@ -19,8 +52,21 @@ function App() {
         <div className="devices_section">
           <h1 className="devices_section_heading">Devices</h1>
 
+          {/* Menu Bar */}
+          <div className="menu-bar">
+            {locations.map((location, i) => {
+              return (
+                <LocationChip
+                  key={i}
+                  location={location}
+                  selectedLocation={selectedLocation}
+                  handleLocationSelect={handleLocationSelect}
+                />
+              );
+            })}
+          </div>
           <div className="device_container">
-            {devices.map((device,i) => {
+            {filteredDevices.map((device, i) => {
               return (
                 <DeviceCard key={i} image={device.image} name={device.name} />
               );
